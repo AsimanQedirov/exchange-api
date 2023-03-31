dd789a39f5b9e397a8ff6915799230105ff15200
asiman.qadirov <asiman.qadirov@a2z.az>
2023-03-31 14:26:53 +0400

    api url changed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // api
-const api = "https://api.exchangeratesapi.io/"
+const api = "https://api.apilayer.com/exchangerates_data/convert?"
 const currency = 'https://openexchangerates.org/api/currencies.json'
 // elements
 
-const el_currency_one = document.getElementById('currency_one')
-const el_currency_two = document.getElementById('currency_two')
+const el_currency_from = document.getElementById('currency_one')
+const el_currency_to = document.getElementById('currency_two')
 const amount = document.getElementById('amount');
 const el_btn_calculate = document.getElementById('btn_calculator')
 var result = document.getElementById('result');
@@ -18,20 +18,43 @@ fetch(currency)
         for (let index = 0; index < keys.length; index++) {
             options += `<option value=${keys[index]}>${values[index]}</option>`
         }
-        el_currency_one.innerHTML += options
-        el_currency_two.innerHTML += options
-    })
+        el_currency_from.innerHTML += options
+        el_currency_to.innerHTML += options
+    });
+
 el_btn_calculate.addEventListener('click', function () {
-    let one = el_currency_one.value;
-    let two = el_currency_two.value;
+    const api_key = 'Pd4O3bo2AUvGK1IJzdmW2FleI2gpMaJX'
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", api_key);
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+    let from = el_currency_from.value;
+    let to = el_currency_to.value;
     let amo = amount.value;
-    console.log(one)
-    console.log(two)
-    console.log(amo)
-    fetch(`${api}latest?base=${one}`)
-    .then(response=>response.json())
-    .then(res => {
-        let rate = res.rates[two]
-        result.innerHTML = `${amo} ${one} = ${amo * rate} ${two}`
-    })
+   
+    el_currency_from.style.borderColor = "black";
+    el_currency_to.style.borderColor = "black";
+    amount.style.borderColor = "black";
+
+    if (!from) {
+        el_currency_from.style.borderColor = "red";
+    }
+    if (!to) {
+        el_currency_to.style.borderColor = "red";
+    }
+    if (!amo) {
+        amount.style.borderColor = "red";
+    }
+    if (from && to && amo) {
+        fetch(`${api}to=${to}&from=${from}&amount=${amo}`, requestOptions)
+            .then(response => response.json())
+            .then(res => {
+                let rate = res.result
+                result.innerHTML = `${amo} ${from} = ${rate} ${to}`
+            })
+    }
+
 })
